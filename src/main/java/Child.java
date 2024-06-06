9332c7a4b325af5647307cc1e7be897210b87128
Ali Mahdavi <154345431+AloofAli@users.noreply.github.com>
2024-06-06 19:54:46 +0330

    Update Child.java

--- src/main/java/Child.java
+++ src/main/java/Child.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Child {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Child implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2L;
+    int age=50;
+    String name="saddam hussein Junior";
+    ChildOfChild childOfChild= new ChildOfChild();
 }
