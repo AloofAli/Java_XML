3e39fb15f2b37dd00ea77d8008e38c8d7c8a0382
Ali Mahdavi <154345431+AloofAli@users.noreply.github.com>
2024-06-06 18:35:06 +0330

    Initial commit

---
+++ src/main/java/Father.java
@@ -0,0 +1,47 @@
+import javax.xml.bind.annotation.XmlElement; // Import XmlElement for specifying XML element names
+import javax.xml.bind.annotation.XmlRootElement; // Import XmlRootElement for specifying the root element of the XML
+import java.io.Serial; // Import Serial for the serialVersionUID annotation (Java 14+)
+import java.io.Serializable; // Import Serializable for enabling object serialization
+
+@XmlRootElement // This annotation indicates that this class can be the root element of an XML document
+public class MyObject implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L; // Unique identifier for serialization
+
+    private String name; // Name field to be serialized and deserialized
+    private int age; // Age field to be serialized and deserialized
+
+    public Child child = new Child(); // Child object to be serialized and deserialized
+
+    // Default no-argument constructor
+    public MyObject() {}
+
+    // Constructor to initialize MyObject with name and age
+    public MyObject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @XmlElement // This annotation specifies that the name field should be represented as an XML element
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElement // This annotation specifies that the age field should be represented as an XML element
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "MyObject{name='" + name + "', age=" + age + '}'; // Override toString() to provide a string representation of MyObject
+    }
+}
